238b285abfe874b1bc7c70eb481ffc4bca0d3f7f
LucianaChamma <107868796+LucianaChamma@users.noreply.github.com>
2023-05-07 19:45:15 -0300

    valor conta

--- src/main.js
+++ src/main.js
@@ -3,3 +3,20 @@ const entrarNumero = prompt("Entre um número");
 const dobroNumero = entrarNumero*2
 
 alert("O dobro do número digitado é " + dobroNumero + ".");
+
+
+
+
+
+const entrarValorConta = prompt("Digite o valor da sua conta:");
+
+const percentual = 0.10;
+
+const totalPercent = entrarValorConta * percentual;
+
+const totalConta = parseInt(entrarValorConta)+ parseInt(totalPercent);
+
+alert("O valor total da conta com taxa de serviço é " + totalConta);
+
+
+
